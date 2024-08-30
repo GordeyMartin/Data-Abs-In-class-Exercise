aeac37fa4baf48580fd41b0945b9788a7829eb35
Gordey Danilochkin <gordanshow@gmail.com>
2024-08-29 20:54:05 -0500

    Spelling error fixed

--- ex2.java
+++ ex2.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class ex2 {
-    private static boolean arrayDuble(int[] nums) {
+    private static boolean arrayDouble(int[] nums) {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
             for (int g = i + 1; g < nums.length; g++) {
